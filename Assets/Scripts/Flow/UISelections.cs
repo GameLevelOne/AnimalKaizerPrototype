3565b68885d0597ad1a920ac249386f6db7a1450
Dennis <dennis_sanjaya74@yahoo.com>
2017-03-23 17:33:09 +0700

    modify character select

--- Assets/Scripts/Flow/UISelections.cs
+++ Assets/Scripts/Flow/UISelections.cs
@@ -3,39 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UISelections : MonoBehaviour {
+	public UnityEngine.UI.Text Text_Name ;
+	public UnityEngine.UI.Text Text_HP ;
+	public UnityEngine.UI.Text Text_Attack ;
+	public UnityEngine.UI.Text Text_Ability ;
+
+	string[] sName = new string[]{"Suster Ngesot","Tuyul","Pocong"};
+	string [] sHP = new string[]{"1000","2000","3000"};
+	string[] sAttack = new string[]{"100","200","300"};
+	string[] sAbility = new string[]{"ngesot","nyolong","loncat"};
+	int SelectedIndex = 0;
+
 	public static UISelections Instance;
 	public GameObject Content_Character;
 	float x, distance;
+
+	void Start(){
+		ShowDetails();
+	}
+
 	public void OnBeginDrag(){
 		x = Input.mousePosition.x;
-		print (Input.mousePosition.x);
-		print (Content_Character.transform.position.x);
+		distance = Content_Character.transform.position.x - x;
 
 	}
 	public void OnDrag(){
-		
-		distance = Content_Character.transform.position.x - x;
-//		if (Content_Character.GetComponent<RectTransform> ().anchoredPosition.x > 400 || Content_Character.GetComponent<RectTransform> ().anchoredPosition.x <= -400) {
-//			if(Content_Character.GetComponent<RectTransform> ().anchoredPosition.x > 400) Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2(400,0);
-//			if(Content_Character.GetComponent<RectTransform> ().anchoredPosition.x < -400) Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2(-400,0);
-//			return;
-//		}
-//		else {
-			print ("GO");
-			Content_Character.transform.position = new Vector3 (x + distance, Content_Character.transform.position.y, Content_Character.transform.position.z);
-//		}
+		float temp = Input.mousePosition.x;
+		Content_Character.transform.position = new Vector3 (temp + distance, Content_Character.transform.position.y, Content_Character.transform.position.z);
+
 	}
 
 	public void OnEndDrag(){
-		float x = Content_Character.GetComponent<RectTransform>().anchoredPosition.x;
+		float xx = Content_Character.GetComponent<RectTransform>().anchoredPosition.x;
 
-		if (x >= 400 || (x < 400 && x >= 200)) {
+		if (xx >= 400 || (xx < 400 && xx >= 200)) {
 			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (400, 0);
-		} else if (x < 200 && x >= -200) {
+			SelectedIndex = 0;
+		} else if (xx < 200 && xx >= -200) {
 			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (0, 0);
-		} else if ((x < -200 && x > -400) || x <= -400) {
+			SelectedIndex = 1;
+		} else if ((xx < -200 && xx > -400) || xx <= -400) {
 			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (-400, 0);
+			SelectedIndex = 2;
 		}
+		ShowDetails();
+	}
+
+	void ShowDetails(){
+		Text_Name.text = "Name : "+ sName[SelectedIndex];
+		Text_Attack.text = "Attack : "+sAttack[SelectedIndex];
+		Text_HP.text = "HP : "+sHP[SelectedIndex];
+		Text_Ability.text = "Ability : "+sAbility[SelectedIndex];
 	}
 
 }

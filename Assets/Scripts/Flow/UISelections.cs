1661a250d1211131c72b2d539088f95d88d14e37
Dennis <dennis_sanjaya74@yahoo.com>
2017-03-24 13:59:43 +0700

    added cards

--- Assets/Scripts/Flow/UISelections.cs
+++ Assets/Scripts/Flow/UISelections.cs
@@ -6,16 +6,17 @@ public class UISelections : MonoBehaviour {
 	public UnityEngine.UI.Text Text_Name ;
 	public UnityEngine.UI.Text Text_HP ;
 	public UnityEngine.UI.Text Text_Attack ;
-	public UnityEngine.UI.Text Text_Ability ;
 
-	string[] sName = new string[]{"Suster Ngesot","Tuyul","Pocong"};
-	string [] sHP = new string[]{"1000","2000","3000"};
-	string[] sAttack = new string[]{"100","200","300"};
-	string[] sAbility = new string[]{"ngesot","nyolong","loncat"};
+
 	int SelectedIndex = 0;
 
 	public static UISelections Instance;
 	public GameObject Content_Character;
+	
+	public CharacterSO[] Characters = new CharacterSO[2];
+	
+	CharacterSO SelectedCharacter;
+	
 	float x, distance;
 
 	void Start(){
@@ -36,24 +37,24 @@ public class UISelections : MonoBehaviour {
 	public void OnEndDrag(){
 		float xx = Content_Character.GetComponent<RectTransform>().anchoredPosition.x;
 
-		if (xx >= 400 || (xx < 400 && xx >= 200)) {
-			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (400, 0);
+		if (xx >= 200 || (xx < 200 && xx >= 0)) {
+			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (200, 0);
 			SelectedIndex = 0;
-		} else if (xx < 200 && xx >= -200) {
-			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (0, 0);
+		} else if ((xx < 0 && xx > -200) || xx <= -200) {
+			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (-200, 0);
 			SelectedIndex = 1;
-		} else if ((xx < -200 && xx > -400) || xx <= -400) {
-			Content_Character.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (-400, 0);
-			SelectedIndex = 2;
 		}
 		ShowDetails();
 	}
 
 	void ShowDetails(){
-		Text_Name.text = "Name : "+ sName[SelectedIndex];
-		Text_Attack.text = "Attack : "+sAttack[SelectedIndex];
-		Text_HP.text = "HP : "+sHP[SelectedIndex];
-		Text_Ability.text = "Ability : "+sAbility[SelectedIndex];
+		Text_Name.text = "Name = "+Characters[SelectedIndex].charName;
+		Text_HP.text = "Healh = "+Characters[SelectedIndex].charHealth.ToString();
+		Text_Attack.text = "Power = "+Characters[SelectedIndex].charPower.ToString();
 	}
 
+	
+	public void ButtonSelect_OnClick(){
+		SelectedCharacter = Characters[SelectedIndex];
+	}
 }

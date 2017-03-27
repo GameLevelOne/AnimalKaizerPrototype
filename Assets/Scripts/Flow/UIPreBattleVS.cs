b9d18373bf2676af747c110b90a8081145a43400
Dennis <dennis_sanjaya74@yahoo.com>
2017-03-27 11:58:39 +0700

    no message

---
+++ Assets/Scripts/Flow/UIPreBattleVS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPreBattleVS : MonoBehaviour {
+
+	public Text Title;
+	public GameObject Player;
+
+	public void Show(){
+		Title.text = Player.GetComponent<Character>().charData+" VS. "+"Enemy";
+	}
+}

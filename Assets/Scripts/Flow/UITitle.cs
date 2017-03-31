75e7771d847e0b0c0e09c741a6770015b8682ecf
Helga Wijaya <helga@gamelevelone.com>
2017-03-31 15:02:58 +0700

    title scene - blinking text, fade in out

--- Assets/Scripts/Flow/UITitle.cs
+++ Assets/Scripts/Flow/UITitle.cs
@@ -1,6 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UITitle : MonoBehaviour {
-	public void OnTapAnywhere(){SceneManager.LoadScene("Scene Selection");}
+    public Image blackScreen;
+    float fadeTimer = 0f;
+    bool fadeIn = true;
+    bool startFade = false;
+    
+    void Start()
+    {
+        StartCoroutine(FadeTo(1, 0, false));
+    }
+
+    void Update() {
+        
+    }
+
+	public void OnTapAnywhere(){
+        StartCoroutine(FadeTo(0, 1, true));
+    }
+
+    IEnumerator FadeTo(float start,float end,bool changeScene)
+    {
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / 2)
+        {
+            Color newColor = new Color(0, 0, 0, Mathf.Lerp(start, end, t));
+            blackScreen.color = newColor;
+            yield return null;
+        }
+        if (changeScene) {
+            yield return new WaitForSeconds(2);
+            SceneManager.LoadScene("Scene Selection");
+        }
+    }
+
+    IEnumerator changeScene() {
+        fadeIn = false;
+        startFade = true;
+        yield return new WaitForSeconds(2);
+        startFade = false;
+        SceneManager.LoadScene("Scene Selection");
+    }
 }

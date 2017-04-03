dc2dba4983944e5bc19f891cd478ed306383d8c5
Helga Wijaya <helga@gamelevelone.com>
2017-04-03 15:14:20 +0700

    add pressed sprite in struggle button

--- Assets/Scripts/Game/GameSceneController.cs
+++ Assets/Scripts/Game/GameSceneController.cs
@@ -832,7 +832,7 @@ public class GameSceneController : MonoBehaviour {
         {
             for (int i = 0; i < currRoulette.Length; i++)
             {
-                currRoulette[i].transform.Translate(Vector3.down * 500 * Time.deltaTime, Space.Self);
+                currRoulette[i].transform.Translate(Vector3.down * 1000 * Time.deltaTime, Space.Self);
                 //rouletteAttackParent.transform.Translate(Vector3.down * 40, Space.Self);
             }
         }
